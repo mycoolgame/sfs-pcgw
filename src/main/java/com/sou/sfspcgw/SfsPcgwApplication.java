f9740e20b8579f66ee15cd3ef3d0d25875857ba7
lihongyuan <lihongyuan@souche.com>
2020-07-10 10:35:07 +0800

    Initial commit

---
+++ src/main/java/com/sou/sfspcgw/SfsPcgwApplication.java
@@ -0,0 +1,13 @@
+package com.sou.sfspcgw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfsPcgwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfsPcgwApplication.class, args);
+    }
+
+}
